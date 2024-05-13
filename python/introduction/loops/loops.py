f27e8f9aa42366a930ea021beedca675b55db16c
gmay46 <gmay46@gmail.com>
2024-05-13 09:09:35 -0500

    hacker rank python introduction solutions

---
+++ python/introduction/loops/loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    for num in range(0,n):
+        print(num*num)
+
+#https://www.hackerrank.com/challenges/python-loops/problem
---
+++ python/introduction/print_function/print_function.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    for num in range(1,n+1):
+        print(num, end='')
+
+
+
+#https://www.hackerrank.com/challenges/python-print/problem
