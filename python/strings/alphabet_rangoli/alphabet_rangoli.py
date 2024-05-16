b3dbea3f159b670d983bafbbb7e128c725dddc5d
gmay46 <gmay46@gmail.com>
2024-05-15 21:24:46 -0500

    added comments

--- python/strings/alphabet_rangoli/alphabet_rangoli.py
+++ python/strings/alphabet_rangoli/alphabet_rangoli.py
@@ -3,26 +3,35 @@ def print_rangoli(size):
     placeholder = []
     rangoli = []
     
+    #building out the intial lists e -> e,d,c,b,a
     for x in range(size-1,-1,-1):
         placeholder += alphabet[x]
         rangoli.append(list(placeholder))
 
+    #adding the right hand side of the top
     for x in range(len(rangoli)):
         initial_length = len(rangoli[x]) 
         if len(rangoli[x]) > 1:
             for y in range(len(rangoli[x])-1):
                 rangoli[x].insert(initial_length, rangoli[x][y])
     
+    #need to know the halfway point
     half_size = len(rangoli)
 
+    #cloning the top to the bottom by inserting the lines into the same position
+    #stopping prior to the halfway point
     for x in range(half_size-1):
        rangoli.insert(half_size,rangoli[x])
 
+    #add the interior dashes
     joined_rangoli = []
     for item in rangoli:
         joined_rangoli.append('-'.join(item))
 
+    #check the widest letter to letter line (which is in the middle)
     width = len(joined_rangoli[half_size-1])
+    
+    #add the exterior dashes
     assembled_rangoli = []
     for item in joined_rangoli:
         current_len = len(item)
@@ -31,6 +40,8 @@ def print_rangoli(size):
             assembled_rangoli.append("-"*spaces+item+"-"*spaces)
         else:
             assembled_rangoli.append(item)
+    
+    #
     print("\n".join(assembled_rangoli))
 
 if __name__ == '__main__':
